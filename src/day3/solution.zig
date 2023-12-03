10acc82a402177c2963f6e25291abfbe5bc57ebe
Stefan Su <stefansu28@gmail.com>
2023-12-03 11:12:50 -0500

    day3 part2

--- src/day3/solution.zig
+++ src/day3/solution.zig
@@ -84,8 +84,93 @@ pub fn part1(reader: Reader) !u64 {
     return sum;
 }
 
-pub fn part2(reader: Reader) !u64 {
-    _ = reader;
+const Pos = struct {
+    x: usize,
+    y: usize,
+};
+const GearInfo = struct {
+    count: u8,
+    product: u64,
+};
+const GearMap = std.AutoHashMap(Pos, GearInfo);
 
-    @panic("TODO");
+fn scanForGear(slice: []const u8) ?usize {
+    for (slice, 0..) |c, n| {
+        if (c == '*') return n;
+    }
+
+    return null;
+}
+
+pub fn part2(reader: Reader) !u64 {
+    var arena_state = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena_state.deinit();
+    const arena = arena_state.allocator();
+
+    var lines = std.ArrayList([]u8).init(arena);
+    var gearMap = GearMap.init(arena);
+
+    var sum: u64 = 0;
+
+    while (try reader.readUntilDelimiterOrEofAlloc(arena, '\n', 256)) |line| {
+        (try lines.addOne()).* = line;
+    }
+
+    for (lines.items, 0..) |line, row| {
+
+        // std.log.debug("line: {s}", .{line});
+
+        var parser = Parser {.buf = line};
+
+        while (parser.pos < parser.buf.len) {
+            const start = if (parser.pos > 0) parser.pos - 1 else parser.pos;
+
+            if (parser.number()) |as_usize| {
+                // can they be next to multiple gears?
+                var gearOpt: ?Pos = null;
+                const num: u64 = @intCast(as_usize);
+
+                const end = parser.pos;
+                const endPlus1 = @min(end+1, parser.buf.len);
+
+                const startChar = line[start];
+                const endChar = line[@min(end, parser.buf.len - 1)];
+                if (startChar == '*') {
+                    gearOpt = .{.x = start, .y = row};
+                } else if (endChar == '*') {
+                    gearOpt = .{.x = end, .y = row};
+                }
+
+                if (row > 0 and gearOpt == null) {
+                    const prev = lines.items[row - 1][start..endPlus1];
+                    if (scanForGear(prev)) |x_offset| {
+                        gearOpt = .{.x = start + x_offset, .y = row - 1};
+                    }
+                }
+                if (row < lines.items.len - 1 and gearOpt == null) {
+                    const next = lines.items[row + 1][start..endPlus1];
+                    if (scanForGear(next)) |x_offset| {
+                        gearOpt = .{.x = start + x_offset, .y = row + 1};
+                    }
+                }
+
+                if (gearOpt) |gear| {
+                    const result = try gearMap.getOrPut(gear);
+                    result.value_ptr.count = if (result.found_existing) result.value_ptr.count + 1 else 1;
+                    result.value_ptr.product = if (result.found_existing) result.value_ptr.product * num else num;
+                }
+            } else {
+                parser.pos += 1;
+            }
+        }
+    }
+
+    var gearItr = gearMap.valueIterator();
+    while (gearItr.next()) |gearInfo| {
+        if (gearInfo.count == 2) {
+            sum += gearInfo.product;
+        }
+    }
+
+    return sum;
 }
