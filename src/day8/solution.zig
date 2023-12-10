996acb8669149b0f997b2946c217d1efc3e45822
Stefan Su <stefansu28@gmail.com>
2023-12-10 13:38:59 -0500

    day8

--- src/day8/solution.zig
+++ src/day8/solution.zig
@@ -19,9 +19,9 @@ pub fn part1(reader: Reader) !u64 {
     const dirs = try reader.readUntilDelimiter(&dirBuf, '\n');
     var dirIndex: usize = 0;
 
-    var network = NodeNetwork.init(arena);
+    // std.log.debug("{s}", .{dirs});
 
-    var initialNode: ?ID = null;
+    var network = NodeNetwork.init(arena);
 
     var buf: [32]u8 = undefined;
     while (try reader.readUntilDelimiterOrEof(&buf, '\n')) |line| {
@@ -45,8 +45,6 @@ pub fn part1(reader: Reader) !u64 {
         std.mem.copy(u8, &node.right, right);
 
         try network.put(id, node);
-
-        if (initialNode == null) initialNode = id;
     }
 
     // var iter = network.iterator();
@@ -56,7 +54,8 @@ pub fn part1(reader: Reader) !u64 {
     //     std.log.debug("{s} = ({s} {s})", .{id, node.left, node.right});
     // }
 
-    var currentNode: ID = initialNode.?;
+    var currentNode: ID = undefined;
+    std.mem.copy(u8, &currentNode, "AAA");
     while (!std.mem.eql(u8, &currentNode, "ZZZ")) {
         const node = network.get(currentNode).?;
         const dir = dirs[dirIndex % dirs.len];
@@ -77,7 +76,79 @@ pub fn part1(reader: Reader) !u64 {
 }
 
 pub fn part2(reader: Reader) !u64 {
-    _ = reader;
+    var arena_state = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena_state.deinit();
+    const arena = arena_state.allocator();
 
-    @panic("TODO");
+    var dirBuf: [512]u8 = undefined;
+    const dirs = try reader.readUntilDelimiter(&dirBuf, '\n');
+    // var dirIndex: usize = 0;
+
+    // std.log.debug("{s}", .{dirs});
+
+    var network = NodeNetwork.init(arena);
+
+    var startNodesBuf: [8]ID = undefined;
+    var currentNodes: []ID = &startNodesBuf;
+    currentNodes.len = 0;
+
+    var buf: [32]u8 = undefined;
+    while (try reader.readUntilDelimiterOrEof(&buf, '\n')) |line| {
+        if (line.len == 0) continue;
+
+        var parser = Parser { .buf = line };
+        const idSlice = parser.until(' ');
+        if (idSlice.len != 3) return error.ParseError;
+        var id: ID = undefined;
+        std.mem.copy(u8, &id, idSlice);
+        if (!parser.maybeSlice(" = (")) return error.ParseError;
+
+        if (id[2] == 'A') {
+            std.mem.copy(u8, &startNodesBuf[currentNodes.len], idSlice);
+            currentNodes.len += 1;
+        }
+
+        const left = parser.until(',');
+        _ = parser.char(); // consume ',';
+        parser.eatSpaces();
+        const right = parser.until(')');
+
+        // std.log.debug("{s} = ({s} {s})", .{id, left, right});
+        var node: Node = undefined;
+        std.mem.copy(u8, &node.left, left);
+        std.mem.copy(u8, &node.right, right);
+
+        try network.put(id, node);
+    }
+
+    for (currentNodes) |current| {
+        std.log.debug("{s}", .{current});
+    }
+
+    var steps: [8]usize = [_]usize {1} ** 8;
+    for (currentNodes, 0..) |*currentNode, n| {
+        var dirIndex: usize = 0;
+        while (currentNode[2] != 'Z') {
+            const node = network.get(currentNode.*).?;
+            const dir = dirs[dirIndex % dirs.len];
+            // std.log.debug("{s} = ({s} {s})", .{currentNode, node.left, node.right});
+            // std.log.debug("dir: {s}, index: {}", .{if (dir == 'L') "left" else "right", dirIndex});
+            if (dir == 'L') {
+                currentNode.* = node.left;
+            } else if (dir == 'R') {
+                currentNode.* = node.right;
+            } else {
+                unreachable;
+            }
+
+            dirIndex += 1;
+        }
+
+        steps[n] = dirIndex;
+    }
+
+    // compute lcm...
+    std.log.debug("{any}", .{steps});
+
+    return 0;
 }
